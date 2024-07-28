3c2f27180dbfd2ee66a0ec268418e064b6410c74
ZhiweiWei-NAMI <2311769@tongji.edu.cn>
2024-07-28 19:23:26 +0800

    添加元类，保证scheduler的方法必须是静态，且以add set get delete开头

--- airfogsim/scheduler/__init__.py
+++ airfogsim/scheduler/__init__.py
@@ -1,2 +1,4 @@
 from .computation_sched import ComputationScheduler
 from .communication_sched import CommunicationScheduler
+from .base_sched import BaseScheduler
+from .reward_sched import RewardScheduler
