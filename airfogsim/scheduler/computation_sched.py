e1e68ed30bda19b9d51a76fd9092b7d570ccf9ec
ZhiweiWei-NAMI <2311769@tongji.edu.cn>
2024-07-28 18:36:03 +0800

    修改部分接口逻辑，scheduler必须传输env

--- airfogsim/scheduler/computation_sched.py
+++ airfogsim/scheduler/computation_sched.py
@@ -1,15 +1,16 @@
 from ..entities.fog_node import FogNode
 from ..airfogsim_env import AirFogSimEnv
-class ComputationScheduler:
+class ComputationScheduler():
     """The computation scheduler for the fog nodes. Provide static methods to schedule the computation tasks for the fog nodes.
     """
 
-    @staticmethod
-    def scheduleCPUbyFogNodeName(env: AirFogSimEnv, fog_node_name: str, resource_allocation: list):
+    def __init__(self, env: AirFogSimEnv):
+        self.env = env
+
+    def scheduleCPUbyFogNodeName(fog_node_name: str, resource_allocation: list):
         """Schedule the computation resources of the fog node (e.g., fog vehicle, edge server, cloud server) by the fog node name.
 
         Args:
-            env (AirFogSimEnv): The environment.
             fog_node_name (str): The fog node name.
             resource_allocation (list): The resource allocation list. Each element is the resource allocation ratio for the computation tasks. Guarantee the sum of the resource allocation ratio is 1.
 
