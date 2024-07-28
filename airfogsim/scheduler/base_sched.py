6171f2d11ad8929c3d2bf053d58cfa8b0ac9aeb7
ZhiweiWei-NAMI <2311769@tongji.edu.cn>
2024-07-28 22:32:40 +0800

    Scheduler更新

--- airfogsim/scheduler/base_sched.py
+++ airfogsim/scheduler/base_sched.py
@@ -1,25 +1,3 @@
-from inspect import isfunction, getfullargspec
-from ..airfogsim_env import AirFogSimEnv
-
-class ValidateStaticMethods(type):
-    def __new__(cls, name, bases, attrs):
-        for key, value in attrs.items():
-            if isfunction(value):  # 检查是否为函数
-                if not isinstance(value, staticmethod):
-                    raise TypeError(f"Method {key} must be a static method")
-                
-                # 检查第一个参数是否为 AirFogSimEnv 类的实例
-                args = getfullargspec(value.__func__).args
-                if len(args) == 0 or args[0] != 'env' or not hasattr(value.__func__, '__annotations__') or value.__func__.__annotations__.get('env') != AirFogSimEnv:
-                    raise TypeError(f"The first argument of method {key} must be 'env: AirFogSimEnv'")
-                
-                # 检查是否以{set, get, add, is, delete}开头，代表增删改查和判断
-                if not key.startswith(('set', 'get', 'add', 'delete', 'is')):
-                    raise ValueError(f"Method {key} must start with 'is', 'set', 'get', 'add', or 'delete'")
-                
-        
-        return super().__new__(cls, name, bases, attrs)
-    
-
+from .validate import ValidateStaticMethods
 class BaseScheduler(metaclass=ValidateStaticMethods):
     pass
---
+++ airfogsim/scheduler/topo_sched.py
@@ -0,0 +1,20 @@
+from .base_sched import BaseScheduler
+from ..airfogsim_env import AirFogSimEnv
+
+class TopologyScheduler(BaseScheduler):
+    """The topology scheduler for the fog nodes. Provide static methods to schedule the topology of the fog nodes.
+    """
+
+    @staticmethod
+    def setTopologyByNodeName(env:AirFogSimEnv, node_name: str, topology: dict):
+        """Schedule the topology of the fog node (e.g., fog vehicle, edge server, cloud server) by the fog node name.
+
+        Args:
+            env (AirFogSimEnv): The environment.
+            fog_node_name (str): The fog node name.
+            topology (dict): The topology of the fog node. The topology includes the connection information of the fog node.
+
+        Returns:
+            bool: The flag to indicate whether the topology is scheduled successfully.
+        """
+        return True
